6cb92b55ddde81397f6034f86aa5db745b88bf7e
Krishnandan2005 <krishnandannitb@gmail.com>
2026-02-02 17:35:27 +0000

    data type

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,40 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber) // nan 
+
+let temp = null
+console.log(temp)
+console.log(Number(temp))
+
+// NOTES 
+// "33" => 33 
+// "33abc" => NaN (Not a Number)
+// true = 1 && false = 0
+
+let isLogedIn = 3
+
+let boolLoggedIn = Boolean(isLogedIn)
+
+console.log(isLogedIn)
+console.log(boolLoggedIn)
+
+let name = "krishna"
+
+let boolname = Boolean(name)
+console.log(name)
+console.log(boolname)
+
+// 1 = true 0=false 
+//""=false "hnhk"=true
+
+let x = 33
+
+let NumbertoString = String(x)
+console.log(NumbertoString)
+console.log(typeof NumbertoString);
+
