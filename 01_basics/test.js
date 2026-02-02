a3c3df4b322a79611bdaa73326fceabf31b624d0
Krishnandan2005 <krishnandannitb@gmail.com>
2026-02-02 11:09:21 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,4 @@
+console.log("Hey there , this is Krishnandan 🎯📚")
+console.log("Java Script is just  wow! 😍")
+console.log("I am very excited to learn this new language.")
+console.log("NodeJS is working 🚀")
