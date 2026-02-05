c6a31ee42e8791d93e0619f5fb85f9400852348b
Krishnandan2005 <krishnandannitb@gmail.com>
2026-02-05 06:33:28 +0000

    string

--- 01_basics/11_string.js
+++ 01_basics/11_string.js
@@ -1,3 +1,4 @@
+// primitive string 
 const name = "Krishnandan"
 const surName = "Yadav"
 // console.log(name + surName + " 22 year old boy");
@@ -5,32 +6,45 @@ const surName = "Yadav"
 
 // modern Syntax
 console.log(`Hello Everyone this is ${name} ${surName} . I am currently persuing BTech in Electrical Enginnering from MANIT, Bhopal`);
-console.log();
+console.log(typeof name);
+console.log("================================================");
 
 
-// new way to 
-const myName = new String('Krishnandan Kumar')
+// new way to construct object string 
+const myName = new String('Krishnandan kumar')
+console.log(typeof myName);
 console.log(myName.length);
 console.log(myName.toUpperCase());
 console.log(myName.charAt(0));
-console.log(myName.indexOf('K'));
+console.log(myName.indexOf('k'));
+console.log("================================================");
 
-const newString = myName.substring(-8,4)
+// substring  == does not implement negative indexing
+const newString = myName.substring(0,4)
 console.log(newString);
+console.log("================================================");
+
 
 // must read about this
+// slice allow negative indexing 
 const newString2 = myName.slice(-8,17)
 console.log(newString2);
+console.log("================================================");
+
 
 // trim == starting and ending unnecessary space remove kr deta hai
 const yourName = "    Durgesh Bhai    "
 console.log(yourName);
 console.log(yourName.trim());
+console.log("================================================");
+
+
 
 // replace 
 const url ="https://hitesh.com/hitesh%20choudhary"
 console.log(url.replace('%20','-'));
 console.log(url.includes('Sundar'));
+console.log("================================================");
 
 
 
