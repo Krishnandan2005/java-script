d6d896f121c7d8e4c238397c0472b2f4ece00cd5
Krishnandan2005 <krishnandannitb@gmail.com>
2026-02-10 16:31:44 +0530

    loop

---
+++ Control-Flow/07_For_Each_Loop.js
@@ -0,0 +1,56 @@
+const coding = ["JS", "rubby", "Java", "Python", "Cpp"]
+
+// call back function 
+// coding.forEach( function (item) {
+//     console.log(item);
+
+// } )
+// console.log();
+
+
+// using arrow functio 
+// coding.forEach( (val) => {
+//     console.log(val);
+
+// } )
+// console.log();
+
+
+// function print(item){
+//     console.log(item);
+
+// }
+// coding.forEach(print)
+// console.log();
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item,index,arr);
+
+// })
+// console.log();
+
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        fileName: ".js"
+    },
+    {
+        languageName: "Java",
+        fileName: ".J"
+    },
+    {
+        languageName: "C++",
+        fileName: ".cpp"
+    },
+    {
+        languageName: "Python",
+        fileName: ".py"
+    }
+]
+myCoding.forEach( (item) => {
+    console.log(item.fileName);
+    
+})
+
+
