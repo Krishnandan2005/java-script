da8e17688d5f05befed159d40e2cd1e1559c7c50
Krishnandan2005 <krishnandannitb@gmail.com>
2026-02-13 10:23:09 +0530

    loop

--- Control-Flow/08_For_Each.js
+++ Control-Flow/08_For_Each.js
@@ -9,17 +9,19 @@
 
 const nums = [1,2,3,4,5,6,7,8,9,10]
 
+// filter -->> works on true / false condition basis
 // const newNums = nums.filter( (num) => num >=4 )
 // console.log(newNums);
 
+
+// as we have defined the scope "{}" so we must use "return" keyword 
 // const newNums = nums.filter( (num) => {
-//     // as we have defined the scope "{}" so we must use "return" keyword 
 //         return num >=4 
 // } )
 // console.log(newNums);
 
 
-// // doing same thing by For Each 
+// // doing same thing by For Each loop
 // const newNums = []
 // nums.forEach( (num) => {
 //     if(num > 4 ){
